6115f5f977e3f40ca7ce2b4d8994ad371c472d53
Eddishot <Eddi_sl94@hotmail.it>
2021-11-11 18:45:00 +0100

    commit

---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+           {{ text}}
+        </h1>
+    </div>
+</template>
+
+
+<script>
+    export default{
+        name: "Title".
+        props:{
+            text: String,
+        }
+    }
+</script
